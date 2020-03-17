5faca6c62c4d7df37aff40073670ac2feb1b5bcd
Jesper Deleurand <jesperd55@gmail.com>
2020-03-17 14:36:18 +0100

    Initial commit

---
+++ TyggegumiMaskine/GumMachine.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TyggegumiMaskine
+{
+    public class GumMachine
+    {
+		private static GumMachine instance;
+
+		private bool dispenserempty;
+
+		public bool DispenserEmpty
+		{
+			get { return dispenserempty; }
+			set { dispenserempty = value; }
+		}
+
+		private double redgum;
+
+		public double RedGum
+		{
+			get { return redgum; }
+			set { redgum = value; }
+		}
+
+		private double bluegum;
+
+		public double BlueGum
+		{
+			get { return bluegum; }
+			set { bluegum = value; }
+		}
+
+		private double greengum;
+
+		public double GreenGum
+		{
+			get { return greengum; }
+			set { greengum = value; }
+		}
+
+		private double yellowgum;
+
+		public double YellowGum
+		{
+			get { return yellowgum; }
+			set { yellowgum = value; }
+		}
+
+		private double purplegum;
+
+		public double PurpleGum
+		{
+			get { return purplegum; }
+			set { purplegum = value; }
+		}
+
+		private double orangegum;
+
+		public double OrangeGum
+		{
+			get { return orangegum; }
+			set { orangegum = value; }
+		}
+
+
+		private GumMachine()
+		{
+			redgum = 7.7;
+			greengum = 5.5;
+			bluegum = 13.75;
+			orangegum = 10.45;
+			purplegum = 6.6;
+			yellowgum = 11;
+		}
+
+		public static GumMachine Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new GumMachine();
+				}
+				return instance;
+			}
+		}
+
+
+		/// <summary>
+		/// Method to register if the machine is empty
+		/// </summary>
+		public void RegEmpty()
+		{
+			double temp = redgum + greengum + bluegum + orangegum + purplegum + yellowgum;
+
+			if (temp <= 0)
+			{
+				dispenserempty = true;
+			}
+		}
+
+
+		/// <summary>
+		/// method to refill the machine
+		/// </summary>
+		public void Refill()
+		{
+			redgum = 7.7;
+			greengum = 5.5;
+			bluegum = 13.75;
+			orangegum = 10.45;
+			purplegum = 6.6;
+			yellowgum = 11;
+			dispenserempty = false;
+		}
+	}
+}
---
+++ TyggegumiMaskine/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace TyggegumiMaskine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            bool machineison = true;
+            string choice = string.Empty;
+
+            while (machineison == true)
+            {
+                Console.Clear();
+                Console.WriteLine("TURN THE HANDLE? Y/N");
+                choice = Console.ReadLine();
+                
+                if(choice == "y" || choice == "Y")
+                {
+                    GumMachine.Instance.RegEmpty();
+                    bool gavegum = false;
+                    while (gavegum == false && GumMachine.Instance.DispenserEmpty == false)
+                    {
+                        Random rand = new Random();
+                        int rando = rand.Next(1, 7);
+
+                        if (rando == 1 && GumMachine.Instance.BlueGum != 0)
+                        {
+                            GumMachine.Instance.BlueGum--;
+                            Console.WriteLine("You got a Blue piece of gum");
+                            gavegum = true;
+                        }
+                        else if (rando == 2 && GumMachine.Instance.PurpleGum != 0)
+                        {
+                            GumMachine.Instance.PurpleGum--;
+                            Console.WriteLine("You got a Purple piece of gum");
+                            gavegum = true;
+                        }
+                        else if (rando == 3 && GumMachine.Instance.YellowGum != 0)
+                        {
+                            GumMachine.Instance.YellowGum--;
+                            Console.WriteLine("You got a Yellow piece of gum");
+                            gavegum = true;
+                        }
+                        else if(rando == 4 && GumMachine.Instance.OrangeGum != 0)
+                        {
+                            GumMachine.Instance.OrangeGum--;
+                            Console.WriteLine("You got an Orange piece of gum");
+                            gavegum = true;
+                        }
+                        else if (rando == 5 && GumMachine.Instance.RedGum != 0)
+                        {
+                            GumMachine.Instance.RedGum--;
+                            Console.WriteLine("You got a Red piece of gum");
+                            gavegum = true;
+                        }
+                        else if(rando == 6 && GumMachine.Instance.GreenGum != 0)
+                        {
+                            GumMachine.Instance.GreenGum--;
+                            Console.WriteLine("You got a Green piece of gum");
+                            gavegum = true;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    
+
+                    while (GumMachine.Instance.DispenserEmpty == true)
+                    {
+                        Console.WriteLine("There is no more gum, please press any key after refilling the machine");
+                        Console.ReadKey();
+                        GumMachine.Instance.Refill();
+                    }
+
+
+                }
+                else if( choice == "n" || choice == "N")
+                {
+                    Console.WriteLine("Then why are you here.");
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid input, try again.");
+                }
+
+                Console.ReadKey();
+            }
+            Console.WriteLine("GoodBye.");
+        }
+    }
+}
